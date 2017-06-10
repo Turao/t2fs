a1e7d30391bec12b5802397a4b10f2a057c58739
turao <lenz.arthur@gmail.com>
2017-06-10 15:56:34 -0300

    +debug test file

---
+++ teste/debug.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "t2fs.h"
+#include "utils.h"
+
+int main()
+{
+  char devs[256];
+
+  if(identify2(devs, 256) == ERROR) return ERROR;
+
+  printf("devs: %s\n", devs);
+  return SUCCESS;
+}
